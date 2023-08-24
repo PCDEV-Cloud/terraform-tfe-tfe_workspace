7d1806ea712121ec4739e638c7830926148dbab2
Paweł Cholewa <pawel.cholewa@tsh.io>
2023-08-24 08:13:07 +0200

    Add outputs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "id" {
+  value       = tfe_workspace.this.id
+  description = "description"
+}
+
+output "html_url" {
+  value       = tfe_workspace.this.html_url
+  description = "description"
+}
