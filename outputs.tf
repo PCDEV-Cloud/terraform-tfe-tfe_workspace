658094258c7b304fd880ae6cab92cda9d5d0c5ce
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-12-08 14:16:50 +0100

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,24 @@
 output "id" {
   value       = tfe_workspace.this.id
-  description = "The workspace ID."
+  description = "The workspace's ID."
 }
 
 output "html_url" {
   value       = tfe_workspace.this.html_url
   description = "The URL to the overview page of the workspace."
+}
+
+output "terraform_version" {
+  value       = tfe_workspace.this.terraform_version
+  description = "The version of Terraform used in the workspace."
+}
+
+output "name" {
+  value       = tfe_workspace.this.name
+  description = "Name of the workspace."
+}
+
+output "tags" {
+  value       = tfe_workspace.this.tags
+  description = "A list of workspace's tags."
 }
